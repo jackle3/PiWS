81b5d621dfeac81bdf26c44ad3893ebb515d7815
Jack Le <jackle61333@gmail.com>
2025-03-13 23:33:40 -0700

    nit

--- code/tcp/tests/tcp-uart.c
+++ code/tcp/tests/tcp-uart.c
@@ -25,8 +25,8 @@ static void test_tcp_reliable_delivery(nrf_t *server_nrf, nrf_t *client_nrf)
     trace("Handshaking...\n");
     while (server->state != TCP_ESTABLISHED || client->state != TCP_ESTABLISHED)
     {
-        tcp_do_handshake(server);
-        tcp_do_handshake(client);
+        tcp_server_handshake(server);
+        tcp_client_handshake(client);
     }
 
     trace("Connection established to %s!\n\n", other_addr_str);
