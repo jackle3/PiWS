37b2775b7c25502cf7131536d09ba23cb1594eb9
Jack Le <jackle61333@gmail.com>
2025-03-14 17:58:52 -0700

    nit

--- code/rcp/rcp-datagram.h
+++ code/rcp/rcp-datagram.h
@@ -7,24 +7,24 @@
 /* RCP Datagram structure
  * Contains both the header and payload of an RCP packet
  */
-struct rcp_datagram {
+typedef struct rcp_datagram {
     struct rcp_header header;  /* RCP header */
     uint8_t* payload;         /* Pointer to payload data */
-};
+} rcp_datagram_t;
 
 /* Initialize a new RCP datagram with default header values */
-struct rcp_datagram rcp_datagram_init(void);
+rcp_datagram_t rcp_datagram_init(void);
 
 /* Parse raw network data into an RCP datagram
  * Returns number of bytes parsed, or -1 on error
  * Note: Allocates memory for payload, caller must free with rcp_datagram_free */
-int rcp_datagram_parse(struct rcp_datagram* dgram, const void* data, size_t length);
+int rcp_datagram_parse(rcp_datagram_t* dgram, const void* data, size_t length);
 
 /* Serialize an RCP datagram into network data
  * Returns number of bytes written, or -1 on error */
-int rcp_datagram_serialize(const struct rcp_datagram* dgram, void* data, size_t max_length);
+int rcp_datagram_serialize(const rcp_datagram_t* dgram, void* data, size_t max_length);
 
 /* Set the payload of an RCP datagram
  * Makes a copy of the provided data
  * Returns 0 on success, -1 on error */
-int rcp_datagram_set_payload(struct rcp_datagram* dgram, const void* data, size_t length);
+int rcp_datagram_set_payload(rcp_datagram_t* dgram, const void* data, size_t length);
